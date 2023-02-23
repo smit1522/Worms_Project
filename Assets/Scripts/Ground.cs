6d854a615ce322f560154474a36c6c9e69766b05
Griffin Smith <griffin.123@comcast.net>
2023-02-22 19:58:52 -0500

    Added Ground Script and Started Player script

---
+++ Assets/Scripts/Ground.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ground : MonoBehaviour
+{
+
+    [SerializeField]
+    private Texture2D baseTexture;
+
+    private Texture2D cloneTexture;
+
+    private SpriteRenderer spriteRenderer;
+
+    private float _widthWorld, _heightWorld;
+    private float _widthPixel, _heightPixel;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        cloneTexture = Instantiate(baseTexture);
+        cloneTexture.alphaIsTransparency = true;
+
+        UpdateTexture();
+
+        gameObject.AddComponent<PolygonCollider2D>();
+    }
+
+    public float WidthWorld
+    {
+        get 
+        {
+            if(_widthWorld == 0)
+                _widthWorld = spriteRenderer.bounds.size.x;
+
+            return _widthWorld;
+        }
+    }
+
+    public float HeightWorld
+    {
+        get 
+        {
+            if(_heightWorld == 0)
+                _heightWorld = spriteRenderer.bounds.size.y;
+
+            return _heightWorld;
+        }
+    }
+
+    public float WidthPixel
+    {
+        get 
+        {
+            if(_widthPixel == 0)
+                _widthPixel = spriteRenderer.sprite.texture.width;
+
+            return _widthPixel;
+        }
+    }
+
+    public float HeightPixel
+    {
+        get 
+        {
+            if(_heightPixel == 0)
+                _heightPixel = spriteRenderer.sprite.texture.height;
+
+            return _heightPixel;
+        }
+    }
+
+void UpdateTexture()
+{
+
+    spriteRenderer.sprite = Sprite.Create(cloneTexture, new Rect(0, 0, cloneTexture.width, cloneTexture.height), new Vector2(0.5f, 0.5f), 50f);
+}
+
+    
+} // Class
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,27 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+
+    [SerializeField]
+    private Rigidbody2D bulletPrefab;
+
+    [SerializeField]
+    private Transform currentGun;
+
+    public float walkSpeed = 1f;
+    public float maxRelativeVelocity = 6f;
+    public float missileForce = 5f;
+
+    public int playerID;
+
+    private SpriteRenderer spriteRenderer;
+
+    private Camera mainCam;
+
+    // public bool IsTurn {}
+    // WormHealth wormHealth;
+
+    
     // Start is called before the first frame update
     void Start()
     {
