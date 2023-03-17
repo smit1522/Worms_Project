54ca23472f914051d5b84ce2ae81532ff487062a
Griffin Smith <griffin.123@comcast.net>
2023-03-17 17:14:25 -0400

    Added Animations IDLE/WALK

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,6 +11,21 @@ public class Player : MonoBehaviour
     [SerializeField]
     private Transform currentGun;
 
+    PlayerStates CurrentState {
+        set {
+            m_currentState = value;
+
+            switch(m_currentState) {
+                case PlayerStates.IDLE:
+                    m_animator.Play("Idle");
+                    break;
+                case PlayerStates.WALK:
+                    m_animator.Play("Walk");
+                    break;
+            }
+        }
+    }
+
     public float walkSpeed = 1f;
     public float maxRelativeVelocity = 6f;
     public float missileForce = 5f;
@@ -27,6 +42,16 @@ public class Player : MonoBehaviour
 
     private Vector3 diff;
 
+    // Animation
+    public enum PlayerStates {
+        IDLE,
+        WALK
+    }
+
+    PlayerStates m_currentState;
+
+    private Animator m_animator;
+
     // Audio
     public AudioSource shootSound;
 
@@ -38,6 +63,8 @@ public class Player : MonoBehaviour
 
         // get health script
         squirrelHealth = GetComponent<SquirrelHealth>();
+
+        m_animator = GetComponent<Animator>();
 
         mainCam = Camera.main;
 
@@ -70,6 +97,11 @@ public class Player : MonoBehaviour
 
         if(hor == 0) {
 
+            CurrentState = PlayerStates.IDLE;
+
+            m_animator.SetFloat("xMove", hor);
+            // m_animator.SetFloat("yMove", hor.y);
+
             currentGun.gameObject.SetActive(true);
 
             if (Input.GetKeyDown (KeyCode.E)) {
@@ -82,8 +114,17 @@ public class Player : MonoBehaviour
                 }
             }
 
+            if(hor > 0) {
+                spriteRenderer.flipX = false;
+            } else if (hor < 0) {
+                spriteRenderer.flipX = true;
+            }
+
         }
         else {
+
+            CurrentState = PlayerStates.WALK;
+
             currentGun.gameObject.SetActive(false);
 
             transform.position += Vector3.right * hor * Time.deltaTime * walkSpeed;
