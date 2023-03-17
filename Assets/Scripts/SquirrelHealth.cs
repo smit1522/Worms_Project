d88c3b9df9af07d8c9e29e6b7ca0842302240817
Griffin Smith <griffin.123@comcast.net>
2023-03-17 15:14:44 -0400

    Added Game Over Screen

--- Assets/Scripts/SquirrelHealth.cs
+++ Assets/Scripts/SquirrelHealth.cs
@@ -7,9 +7,26 @@ using TMPro;
 
 public class SquirrelHealth : MonoBehaviour
 {
+    // GameObjects
+    public GameObject player;
+
+
+    // Health Section
 
     private int health;
     public int maxHealth = 3;
+
+    // GameOver section
+
+    public float fadeDuration = 1f;
+    public float displayImageDuration = 1f;
+
+    public CanvasGroup deadBackgroundImageCanvasGroup;
+
+    public AudioSource gameOverAudio;
+
+    bool m_HasAudioPlayed;
+    float m_Timer;
 
     // might need TXTMeshPro here
     [SerializeField]
@@ -20,7 +37,21 @@ public class SquirrelHealth : MonoBehaviour
     {
         health = maxHealth;
         healthTxt.text = health.ToString();
+
+        player = GameObject.Find("player");
+
     }
+
+    void Update ()
+    {
+        
+        // if (player.gameObject == null)
+        // {
+        //     EndLevel (deadBackgroundImageCanvasGroup, false, gameOverAudio);
+        // }
+        
+    }
+
 
     public void ChangeHealth(int change) {
 
@@ -33,13 +64,32 @@ public class SquirrelHealth : MonoBehaviour
         else if(health <= 0)
         {
             health = 0;
-            Destroy(this.gameObject);
+            //Destroy(this.gameObject);
+            player.SetActive(false);
+            EndLevel (deadBackgroundImageCanvasGroup, false, gameOverAudio);
         }
 
         // might need TXTMeshPro here
         healthTxt.text = health.ToString();
     }
 
+    // Game Ending screen when health is 0
+    void EndLevel (CanvasGroup deadBackgroundImageCanvasGroup, bool doRestart, AudioSource audioSource)
+    {
+        m_Timer += Time.deltaTime;
+
+        deadBackgroundImageCanvasGroup.alpha = m_Timer / fadeDuration;
+
+        if(m_Timer > fadeDuration + displayImageDuration)
+        {
+            Application.Quit ();
+        }
+    }
+
+    // public void OnDestroy()
+    // {
+    //     EndLevel (deadBackgroundImageCanvasGroup, false, gameOverAudio);
+    // }
 
 
 } //class
